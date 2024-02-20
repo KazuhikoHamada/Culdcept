d4932715b867fbca69827ea3b558138d182ce51c
K.Hamada <k.hamadas55@gmail.com>
2024-02-20 10:07:07 +0900

    LandTypeを追加する。

--- Assets/Project/Battle/Scripts/Enums/LandType.cs
+++ Assets/Project/Battle/Scripts/Enums/LandType.cs
@@ -1,8 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public enum LandType
 {
-
+    /// <summary>
+    /// 無
+    /// </summary>
+    None,
+    /// <summary>
+    /// ゲート
+    /// </summary>
+    Gate,
+    /// <summary>
+    /// 火
+    /// </summary>
+    Fire,
+    /// <summary>
+    /// 水
+    /// </summary>
+    Water,
+    /// <summary>
+    /// 土
+    /// </summary>
+    Soil,
+    /// <summary>
+    /// 風
+    /// </summary>
+    Wind,
 }
