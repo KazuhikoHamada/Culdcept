ccbb0797fbbe8b8e78aa864942a3e81b83adc1ab
K.Hamada <k.hamadas55@gmail.com>
2024-02-21 12:05:39 +0900

    土地の画像を追加する。

--- Assets/Project/Battle/Scripts/View/LandView.cs
+++ Assets/Project/Battle/Scripts/View/LandView.cs
@@ -1,11 +1,22 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public sealed class LandView : MonoBehaviour
 {
+    [SerializeField] public LandEntity LandEntity = new LandEntity();
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] bool _isGoal;
+    [SerializeField] LandView _nextLandView;
     [SerializeField] List<LandSpriteView> _landSpriteViewList;
 
-    [NonSerialized] public LandEntity LandEntity = new LandEntity();
+    void OnValidate()
+    {
+        var sprite = _landSpriteViewList.Where(n => n.LandType == LandEntity.LandType).Select(n => n.Sprite).FirstOrDefault();
+
+        if (sprite != null)
+        {
+            _spriteRenderer.sprite = sprite;
+        }
+    }
 }
